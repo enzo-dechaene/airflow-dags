5563bc375c72915edda6b922a170e95ccaddfb76
Enzo DECHAENE <enzo.dechaene@kosmos.fr>
2023-04-14 16:16:56 -0400

    update k8s code python

--- kubernetes.py
+++ kubernetes.py
@@ -13,28 +13,51 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+configmaps = [
+    k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name="test-configmap-1")),
+    k8s.V1EnvFromSource(config_map_ref=k8s.V1ConfigMapEnvSource(name="test-configmap-2")),
+]
+
 environments = [
     k8s.V1EnvVar(name="PULSAR_URL", value="pulsar://172.16.106.50:30767"), 
     k8s.V1EnvVar(name="PULSAR_INPUT_MESSAGE", value="GENERATION_ETABLISSEMENT"),
     k8s.V1EnvVar(name="PULSAR_OUTPUT_MESSAGE", value="GENERATION_ETABLISSEMENT_OK")
 ]
 
-dag = DAG(
-    'kubernetes_pod_dag',
+
+with DAG(
+    DAG_ID,
     default_args=default_args,
-    schedule_interval=timedelta(days=1),
-)
+    description="pulsar",
+    schedule=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
+    tags=["example", "cncf", "kubernetes"],
+    catchup=False,
+) as dag:
+    t1 = KubernetesPodOperator(
+        dag=dag
+        name='send_message_pulsar',
+        namespace='airflow',
+        image='harbor.knada.rancher.kosmos.fr/public/send_message_pulsar:v1.0.11',
+        image_pull_secrets=[k8s.V1LocalObjectReference("harbor-knada-credential")],
+        env_from=environments,
+        is_delete_operator_pod=True,
+        get_logs=True,
+        in_cluster=True,
+        task_id="task-one",
+    )
 
-create_pod = KubernetesPodOperator(
-    dag=dag,
-    task_id='create_pod',
-    name='send_message_pulsar',
-    image='harbor.knada.rancher.kosmos.fr/public/send_message_pulsar:v1.0.11',
-    cmds=['python', 'my_script.py'],
-    env=environments,
-    namespace='airflow',
-    is_delete_operator_pod=True,
-    get_logs=True,
-)
+    t2 = KubernetesPodOperator(
+        dag=dag
+        name='send_message_pulsar',
+        namespace='airflow',
+        image='harbor.knada.rancher.kosmos.fr/public/send_message_pulsar:v1.0.11',
+        image_pull_secrets=[k8s.V1LocalObjectReference("harbor-knada-credential")],
+        env_from=environments,
+        is_delete_operator_pod=True,
+        get_logs=True,
+        in_cluster=True,
+        task_id="task-two",
+    )
 
-create_pod
+    t1 >> t2
